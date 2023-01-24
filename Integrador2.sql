f8258679a350c1c1ecb7edb98e0993b3b59a51be
Pablo <pablomonardo@gmail.com>
2023-01-23 22:08:42 -0300

    Muestreo sesgado

--- Integrador2.sql
+++ Integrador2.sql
@@ -8,3 +8,13 @@ SELECT ARTISTA, TITULO, GENERO FROM TOP_SPOTIFY;
 SELECT ARTISTA, TITULO, GENERO FROM TOP_SPOTIFY ORDER BY GENERO;
 -- Lo mismo, en caso de repetirse genero ordenarlo en base al artista
 SELECT ARTISTA, TITULO, GENERO FROM TOP_SPOTIFY ORDER BY GENERO, ARTISTA;
+
+-- Segunda parte
+-- 10 canciones, ordenadas alfabeticamente por artistas y titulo.
+SELECT ARTISTA, TITULO, GENERO FROM TOP_SPOTIFY ORDER BY ARTISTA, TITULO LIMIT 10;
+-- Lo mismo, pero desde la 11 a la 15
+SELECT ARTISTA, TITULO, GENERO FROM TOP_SPOTIFY ORDER BY ARTISTA, TITULO LIMIT 4 OFFSET 10;
+-- Canciones de madonna
+SELECT * FROM TOP_SPOTIFY WHERE ARTISTA = 'Madonna';
+-- Canciones de pop ordenadas en funcion al titulo
+SELECT * FROM TOP_SPOTIFY WHERE GENERO = 'Pop' ORDER BY TITULO;
