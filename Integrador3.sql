f894a2c51f7560ac7cdc04c13a8a447a42816496
Pablo <pablomonardo@gmail.com>
2023-01-24 15:55:58 -0300

    Update Integrador3.sql

--- Integrador3.sql
+++ Integrador3.sql
@@ -3,4 +3,18 @@ USE bonus_track;
 SELECT CONCAT(ARTISTA, ' - ' , TITULO) AS CANCION, GENERO
 FROM TOP_SPOTIFY
 ORDER BY CANCION;
--- 
+-- Alternativa a lo anterior
+SELECT CONCAT_WS(' - ' ,ARTISTA, TITULO) AS CANCION, GENERO
+FROM TOP_SPOTIFY
+ORDER BY CANCION;
+-- Generos en mayuscula
+SELECT CONCAT(ARTISTA, ' - ' , TITULO) AS CANCION, UPPER(GENERO)
+FROM TOP_SPOTIFY
+ORDER BY CANCION;
+-- Lo anterior, pero con una columna que calcule cuanto tiempo tiene la cancion
+SELECT CONCAT(ts.ARTISTA, ' - ' , ts.TITULO) AS CANCION, 
+	ts.GENERO,
+    YEAR(curdate()) - ts.ANO AS 'AÑOS'
+FROM TOP_SPOTIFY as ts
+ORDER BY CANCION;
+
