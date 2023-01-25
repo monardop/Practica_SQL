a6a5bd48794bd0e8dbd741579ceffd0f52169fc4
Pablo <pablomonardo@gmail.com>
2023-01-25 14:05:54 -0300

    Update EjecucionConsultas.sql

--- ProyectoIntegrador/EjecucionConsultas.sql
+++ ProyectoIntegrador/EjecucionConsultas.sql
@@ -5,6 +5,17 @@ FROM laboratorio.clientes_neptuno
 ORDER BY Pais, Ciudad
 LIMIT 5 OFFSET 10;
 
+SELECT * FROM laboratorio.clientes_neptuno
+WHERE Pais LIKE '%Argentina%';
+
+SELECT * FROM laboratorio.clientes_neptuno 
+WHERE Pais NOT LIKE '%Argentina%'
+ORDER BY Pais;
+
+SELECT * FROM laboratorio.clientes_neptuno
+WHERE PAIS IN('Argentina', 'Venezuela', 'Brasil')
+ORDER BY Pais, Ciudad;
+
 -- Tabla de nacimientos
 -- Bebes de madres extranjeras:
 SELECT * FROM laboratorio.nacimientos
@@ -23,3 +34,13 @@ WHERE EDAD_MADRE = EDAD_PADRE;
 SELECT * FROM laboratorio.nacimientos
 WHERE EDAD_PADRE - EDAD_MADRE >= 40
 ORDER BY EDAD_PADRE DESC;
+
+SELECT * FROM laboratorio.nacimientos
+WHERE SEMANAS < 20
+ORDER BY SEMANAS;
+
+SELECT * FROM laboratorio.nacimientos
+WHERE SEXO LIKE '%Femenino%' AND
+	NACIONALIDAD LIKE '%Extranjera%' AND
+    ESTADO_CIVIL_MADRE LIKE '%Soltera%' and
+    EDAD_MADRE > 40;
