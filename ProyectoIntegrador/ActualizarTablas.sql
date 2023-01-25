cb7a41753e6d7c6fef5fdd8960033bfb1295e653
Pablo <pablomonardo@gmail.com>
2023-01-25 19:48:13 -0300

    Cambio de nombre

---
+++ ProyectoIntegrador/ActualizarTablas.sql
@@ -0,0 +1,12 @@
+USE laboratorio;
+SET SQL_SAFE_UPDATES = 0;
+
+ALTER TABLE laboratorio.empleados
+ADD COLUMN NombreEmpleado VARCHAR(30) AFTER IDEmpleado;
+
+UPDATE laboratorio.empleados
+SET NombreEmpleado = CONCAT(Apellidos, ', ', Nombre);
+
+ALTER TABLE laboratorio.empleados
+DROP Apellidos,
+DROP Nombre;
