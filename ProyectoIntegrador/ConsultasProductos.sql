1adcddc5989cd25a3c516f25a4609ee4de2cf0b8
Pablo <pablomonardo@gmail.com>
2023-01-25 15:55:34 -0300

    Update ConsultasProductos.sql

--- ProyectoIntegrador/ConsultasProductos.sql
+++ ProyectoIntegrador/ConsultasProductos.sql
@@ -5,3 +5,10 @@ SELECT
     MAX(PrecioUnidad) AS 'Precio Máximo'
 FROM laboratorio.productos_neptuno
 GROUP BY NombreCategoria;
+
+SELECT 
+	Seccion,
+	COUNT(Cod_producto) as Cantidad
+FROM laboratorio.productos
+GROUP BY Seccion
+ORDER BY Cantidad;
