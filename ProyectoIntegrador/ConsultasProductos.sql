e6831e5cb093907a2e1395eae073609c53099ddc
Pablo <pablomonardo@gmail.com>
2023-01-25 22:39:58 -0300

    Update ConsultasProductos.sql

--- ProyectoIntegrador/ConsultasProductos.sql
+++ ProyectoIntegrador/ConsultasProductos.sql
@@ -12,3 +12,12 @@ SELECT
 FROM laboratorio.productos
 GROUP BY Seccion
 ORDER BY Cantidad;
+
+-- Precios mas caros que la media
+SELECT * FROM laboratorio.productos_neptuno
+WHERE PrecioUnidad > (SELECT AVG(PrecioUnidad) FROM laboratorio.productos_neptuno)
+ORDER BY NombreProducto;
+
+-- Precios mas caros que productos suspendidos
+SELECT * FROM productos_neptuno
+WHERE PrecioUnidad > (SELECT MAX(PrecioUnidad) FROM ProductoSuspendido);
