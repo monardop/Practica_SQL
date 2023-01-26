132a67f2ee55d45315461acd5242aef3e693269b
Pablo <pablomonardo@gmail.com>
2023-01-25 21:59:47 -0300

    Update ActualizarTablas.sql

--- ProyectoIntegrador/ActualizarTablas.sql
+++ ProyectoIntegrador/ActualizarTablas.sql
@@ -13,3 +13,42 @@ DROP Nombre;
 
 UPDATE laboratorio.clientes_neptuno
 SET Pais = 'USA' WHERE Pais = 'Estados Unidos';
+
+UPDATE laboratorio.clientes_neptuno
+SET NombreCompania = UPPER(NombreCompania);
+
+UPDATE laboratorio.clientes_neptuno
+SET 
+	Ciudad = UPPER(ciudad),
+    Pais = UPPER(Pais);
+
+ALTER TABLE laboratorio.clientes
+ADD COLUMN Tipo VARCHAR(3);
+
+UPDATE laboratorio.clientes
+SET Tipo = 'VIP' 
+WHERE ciudad LIKE '%Madrid%';
+SELECT * FROM clientes;
+
+ALTER TABLE laboratorio.clientes
+MODIFY telefono VARCHAR(20);
+
+UPDATE laboratorio.clientes
+SET telefono = CONCAT('+34-', telefono)
+WHERE telefono IS NOT NULL;
+
+ALTER TABLE laboratorio.productos 
+ADD COLUMN fecha DATE;
+
+UPDATE laboratorio.productos
+SET fecha = CONCAT_WS('-', ano, mes, dia);
+UPDATE laboratorio.productos
+SET origen = 'ESPAÑA' WHERE origen LIKE '%espana%';
+
+ALTER TABLE laboratorio.productos
+DROP dia, 
+DROP mes,
+DROP ano;
+
+select * from productos;
+
