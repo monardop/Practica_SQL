37814f075c70fd2216dd2f72fe4450e75188222c
Pablo <pablomonardo@gmail.com>
2023-02-15 11:06:07 -0300

    Update Consultas4.sql

--- TiendaInformatica/Consultas4.sql
+++ TiendaInformatica/Consultas4.sql
@@ -22,3 +22,58 @@ WHERE precio =
                 WHERE nombre LIKE "%lenovo%"
 			)
 		);
+-- 3
+SELECT 
+	Nombre,
+    MAX(precio) AS Precio
+FROM tienda.producto
+WHERE id_fabricante = 
+	(
+		SELECT id
+        FROM tienda.fabricante
+        WHERE nombre = "Lenovo"
+	);
+-- 4
+SELECT nombre,
+	MIN(precio)
+FROM tienda.producto
+WHERE id_fabricante = 
+	( 
+		SELECT id
+        FROM tienda.fabricante
+        WHERE nombre = "Hewlett-Packard"
+	);
+-- 5
+SELECT *
+FROM tienda.producto
+WHERE precio >= 
+	(
+		SELECT MAX(precio)
+        from tienda.producto
+        where id_fabricante = 
+        (
+			SELECT id
+			FROM tienda.fabricante
+			WHERE nombre = "Lenovo"
+		)
+	);
+-- 6
+SELECT * 
+from tienda.producto
+where id_fabricante = 
+	(
+		select id
+        from tienda.fabricante
+        where nombre = "asus"
+	)
+    and precio > 
+    (
+		select avg(precio)
+        from tienda.producto
+        where id_fabricante = 
+			(
+				select id
+                from tienda.fabricante
+                where nombre = "asus"
+			)
+		);
