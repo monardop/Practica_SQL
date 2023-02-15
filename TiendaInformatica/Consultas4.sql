cad5cc3957ff25d704d9112e0c430c3052a41254
Pablo <pablomonardo@gmail.com>
2023-02-15 18:26:25 -0300

    style: format the queries

--- TiendaInformatica/Consultas4.sql
+++ TiendaInformatica/Consultas4.sql
@@ -1,126 +1,185 @@
 -- 1.1.7.1
 -- 1
-SELECT Nombre
-FROM tienda.producto
-WHERE id_fabricante = 
-	(
-		SELECT id
-		FROM tienda.fabricante
-		WHERE nombre LIKE "%Lenovo%"
-    );	
+SELECT 
+    Nombre
+FROM
+    tienda.producto
+WHERE
+    id_fabricante = (SELECT 
+            id
+        FROM
+            tienda.fabricante
+        WHERE
+            nombre LIKE '%Lenovo%');	
 -- 2
-SELECT * 
-FROM tienda.producto
-WHERE precio = 
-	(
-		SELECT MAX(precio)
-        FROM tienda.producto
-        WHERE id_fabricante = 
-			(
-				SELECT id
-                FROM tienda.fabricante
-                WHERE nombre LIKE "%lenovo%"
-			)
-		);
+SELECT 
+    *
+FROM
+    tienda.producto
+WHERE
+    precio = (SELECT 
+            MAX(precio)
+        FROM
+            tienda.producto
+        WHERE
+            id_fabricante = (SELECT 
+                    id
+                FROM
+                    tienda.fabricante
+                WHERE
+                    nombre LIKE '%lenovo%'));
 -- 3
 SELECT 
-	Nombre,
-    MAX(precio) AS Precio
-FROM tienda.producto
-WHERE id_fabricante = 
-	(
-		SELECT id
-        FROM tienda.fabricante
-        WHERE nombre = "Lenovo"
-	);
+    Nombre, MAX(precio) AS Precio
+FROM
+    tienda.producto
+WHERE
+    id_fabricante = (SELECT 
+            id
+        FROM
+            tienda.fabricante
+        WHERE
+            nombre = 'Lenovo');
 -- 4
-SELECT nombre,
-	MIN(precio)
-FROM tienda.producto
-WHERE id_fabricante = 
-	( 
-		SELECT id
-        FROM tienda.fabricante
-        WHERE nombre = "Hewlett-Packard"
-	);
+SELECT 
+    nombre, MIN(precio)
+FROM
+    tienda.producto
+WHERE
+    id_fabricante = (SELECT 
+            id
+        FROM
+            tienda.fabricante
+        WHERE
+            nombre = 'Hewlett-Packard');
 -- 5
-SELECT *
-FROM tienda.producto
-WHERE precio >= 
-	(
-		SELECT MAX(precio)
-        from tienda.producto
-        where id_fabricante = 
-        (
-			SELECT id
-			FROM tienda.fabricante
-			WHERE nombre = "Lenovo"
-		)
-	);
+SELECT 
+    *
+FROM
+    tienda.producto
+WHERE
+    precio >= (SELECT 
+            MAX(precio)
+        FROM
+            tienda.producto
+        WHERE
+            id_fabricante = (SELECT 
+                    id
+                FROM
+                    tienda.fabricante
+                WHERE
+                    nombre = 'Lenovo'));
 -- 6
-SELECT * 
-from tienda.producto
-where id_fabricante = 
-	(
-		select id
-        from tienda.fabricante
-        where nombre = "asus"
-	)
-    and precio > 
-    (
-		select avg(precio)
-        from tienda.producto
-        where id_fabricante = 
-			(
-				select id
-                from tienda.fabricante
-                where nombre = "asus"
-			)
-		);
+SELECT 
+    *
+FROM
+    tienda.producto
+WHERE
+    id_fabricante = (SELECT 
+            id
+        FROM
+            tienda.fabricante
+        WHERE
+            nombre = 'asus')
+        AND precio > (SELECT 
+            AVG(precio)
+        FROM
+            tienda.producto
+        WHERE
+            id_fabricante = (SELECT 
+                    id
+                FROM
+                    tienda.fabricante
+                WHERE
+                    nombre = 'asus'));
 -- 1.1.7.2
 -- 8
-select * 
-from tienda.producto
-where precio >= All 
-	(
-		select precio 	
-        from tienda.producto
-);
+SELECT 
+    *
+FROM
+    tienda.producto
+WHERE
+    precio >= ALL (SELECT 
+            precio
+        FROM
+            tienda.producto);
 -- 9
-SELECT * 
-from tienda.producto
-where precio <= ALL
-	(
-		select precio
-        from tienda.producto
-);
+SELECT 
+    *
+FROM
+    tienda.producto
+WHERE
+    precio <= ALL (SELECT 
+            precio
+        FROM
+            tienda.producto);
 -- 10
-select nombre
-from tienda.fabricante
-where id = ANY (
-		select id_fabricante
-        from tienda.producto
-);
+SELECT 
+    nombre
+FROM
+    tienda.fabricante
+WHERE
+    id = ANY (SELECT 
+            id_fabricante
+        FROM
+            tienda.producto);
 -- 11
-select nombre
-from tienda.fabricante
-where id <> ALL (
-	select id_fabricante
-    from tienda.producto
-);
+SELECT 
+    nombre
+FROM
+    tienda.fabricante
+WHERE
+    id <> ALL (SELECT 
+            id_fabricante
+        FROM
+            tienda.producto);
 
 -- 1.1.7.3
 -- 12
-select nombre
-from tienda.fabricante
-where id  IN(
-	select id_fabricante
-    from tienda.producto
-);
+SELECT 
+    nombre
+FROM
+    tienda.fabricante
+WHERE
+    id IN (SELECT 
+            id_fabricante
+        FROM
+            tienda.producto);
 -- 13
-select nombre
-from tienda.fabricante
-where id  NOT IN(
-	select id_fabricante
-    from tienda.producto
-)
+SELECT 
+    nombre
+FROM
+    tienda.fabricante
+WHERE
+    id NOT IN (SELECT 
+            id_fabricante
+        FROM
+            tienda.producto);
+
+-- 1.1.7.4
+-- 14
+SELECT 
+    nombre
+FROM
+    tienda.fabricante as f
+WHERE
+    EXISTS( SELECT 
+           id_fabricante
+        FROM
+            tienda.producto AS pr
+        WHERE
+            f.id = pr.id_fabricante);
+-- 15
+SELECT 
+    nombre
+FROM
+    tienda.fabricante as f
+WHERE
+    NOT EXISTS( SELECT 
+           id_fabricante
+        FROM
+            tienda.producto AS pr
+        WHERE
+            f.id = pr.id_fabricante);
+
+
