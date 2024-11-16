931a5b55750722d652fced583fc02d37c45a4e73
Pablo <pablomonardo@gmail.com>
2024-11-16 08:11:27 -0300

    Ejercicio 2: done

--- PracticaWF/Ejercicios.sql
+++ PracticaWF/Ejercicios.sql
@@ -3,8 +3,19 @@ GO
 
 USE PracticaWF
 GO
-
+-- Ejercicio 1: ordenar en torno al mejor salario
 SELECT EmpleadoID, Nombre, Departamento, Salario,
-    RANK() OVER (ORDER BY Salario) AS OrdenEmpleadosSalario
+    RANK() OVER (ORDER BY Salario DESC) AS OrdenEmpleadosSalario
 FROM tablaswf.Empleados
 GO
+
+
+-- Ejercicio 2: ordenar en torno al mejor salario por depto
+SELECT EmpleadoID, Nombre, Departamento, Salario,
+    RANK() OVER (
+        PARTITION BY Departamento 
+        ORDER BY Salario DESC
+    ) AS Ranking
+FROM tablaswf.Empleados
+GO
+
