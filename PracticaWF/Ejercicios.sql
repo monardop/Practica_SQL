16f4a654a93ec86f714316feea445fc2de186446
Pablo <pablomonardo@gmail.com>
2024-11-16 08:42:09 -0300

    Ejercicio 4: done

--- PracticaWF/Ejercicios.sql
+++ PracticaWF/Ejercicios.sql
@@ -26,3 +26,25 @@ SELECT EmpleadoID, Nombre, Departamento, Salario,
     ) AS GrupoSalario
 FROM tablaswf.Empleados
 GO
+
+/*
+Realizar una comparación de salarios entre empleados (del ejercicio anterior) para analizar la 
+diferencia de salario entre el empleado actual y el siguiente, así como el salario del empleado 
+anterior, dentro de un mismo departamento. 
+Generar una consulta que muestre, para cada empleado, su salario actual, el salario del 
+empleado anterior en orden ascendente de salarios dentro del mismo departamento, y el salario 
+del empleado siguiente en el mismo orden de salarios. Esta comparación permitirá visualizar 
+cómo varían los salarios entre los empleados, mostrando la relación de los salarios con respecto 
+al empleado anterior y posterior en términos de monto.
+*/
+SELECT EmpleadoID, Nombre, Departamento, Salario,
+    LAG(Salario,1,0.0) OVER (
+		PARTITION BY departamento
+        ORDER BY Salario
+    )AS SalarioAnterior,
+    LEAD(Salario, 1, 0.0) OVER (
+        PARTITION BY departamento
+        ORDER BY Salario
+    )AS SiguienteSalario
+FROM tablaswf.Empleados
+GO
