72e7ffd53d64bcd7099c380eb93488a8b49077aa
Pablo <pablomonardo@gmail.com>
2024-11-16 13:19:30 -0300

    Ejercicio 6

--- PracticaWF/Ejercicios2.sql
+++ PracticaWF/Ejercicios2.sql
@@ -12,3 +12,20 @@ SELECT id_pedido, id_cliente, monto,
 FROM tablaswf.Pedidos;
 GO
 
+/* 
+Encontrar a los tres principales clientes (por monto total de pedidos) de cada país, mostrando 
+su nombre, país y el monto total de sus pedidos 
+*/
+
+SELECT cl.nombre, cl.pais,
+    SUM(pd.monto) OVER (
+        PARTITION BY cl.pais
+        ORDER BY pd.monto
+    ) AS monto_total_pedidos,
+    RANK() OVER (
+        PARTITION BY cl.pais
+        ORDER BY pd.monto
+    ) AS ranking
+FROM tablaswf.Pedidos pd
+JOIN tablaswf.Clientes cl 
+    ON cl.id_cliente = pd.id_cliente 
