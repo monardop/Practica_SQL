46370f92118bf2d7878a63b1ec591f7ba25bcd72
Pablo <pablomonardo@gmail.com>
2024-11-27 15:53:56 -0300

    Arreglo estilos

--- App Universidad/CrearTablas.sql
+++ App Universidad/CrearTablas.sql
@@ -7,32 +7,33 @@ GO
 USE University;
 
 GO
-
-GO
 CREATE TABLE [career_branch]
 (
-	id INT NOT NULL IDENTITY,
-	branch_description VARCHAR(70) NOT NULL,
-	CONSTRAINT PK_ID_Branch PRIMARY KEY(id)
+	id 					INT 		NOT NULL 	IDENTITY,
+	branch_description 	VARCHAR(70) NOT NULL,
+	CONSTRAINT PK_ID_Branch 
+		PRIMARY KEY(id)
 );
 
 GO
 CREATE TABLE [course_State]
 (
-	id INT NOT NULL IDENTITY,
-	state_name VARCHAR(20) NOT NULL,
-	CONSTRAINT PK_id_course_state PRIMARY KEY(id)
+	id 			INT 		NOT NULL IDENTITY,
+	state_name 	VARCHAR(20) NOT NULL,
+	CONSTRAINT PK_id_course_state 
+		PRIMARY KEY(id)
 );
 
 GO
 CREATE TABLE [course]
 (
-	code INT NOT NULL,
-	course_name VARCHAR(100) NOT NULL,
-	id_branch INT NOT NULL,
-	id_state INT NOT NULL,
-	career_year TINYINT NOT NULL,
+	code 		INT 			NOT NULL,
+	course_name VARCHAR(100) 	NOT NULL,
+	id_branch 	INT 			NOT NULL,
+	id_state 	INT 			NOT NULL,
+	career_year TINYINT 		NOT NULL,
 	mark 		INT,
+	available	BIT 			DEFAULT 0,
 	CONSTRAINT PK_course_code 
 			PRIMARY KEY(code),
 	CONSTRAINT FK_course_state FOREIGN KEY (id_state) 
@@ -44,8 +45,8 @@ CREATE TABLE [course]
 GO
 CREATE TABLE [prerequisite_course]
 (
-	course_code INT NOT NULL,
-	prerequisite_code INT NOT NULL,
+	course_code 		INT		NOT NULL,
+	prerequisite_code 	INT 	NOT NULL,
 	CONSTRAINT PK_prerequisite 
 			PRIMARY KEY(course_code, prerequisite_code),
 	CONSTRAINT FK_course FOREIGN KEY (course_code) 
@@ -57,24 +58,32 @@ CREATE TABLE [prerequisite_course]
 GO
 CREATE TABLE [Origin]
 (
-	id INT IDENTITY,
+	id 			INT 		IDENTITY,
 	origin_type VARCHAR(60) NOT NULL
-		CONSTRAINT PK_Origin PRIMARY KEY(id)
+	CONSTRAINT PK_Origin 
+		PRIMARY KEY(id)
 );
 
 GO
 CREATE TABLE [Academic_History]
 (
-	id INT IDENTITY,
-	log_date date NOT NULL,
-	course_cod INT NOT NULL,
-	mark INT,
-	origin_id INT NOT NULL,
-	CONSTRAINT PK_Academic_History PRIMARY KEY(id),
+	id 			INT 	IDENTITY,
+	log_date 	DATE 	NOT NULL,
+	course_cod 	INT 	NOT NULL,
+	mark 		INT,
+	origin_id 	INT 	NOT NULL,
+	CONSTRAINT PK_Academic_History 
+		PRIMARY KEY(id),
 	CONSTRAINT FK_Origin 
-			FOREIGN KEY (origin_id) 
-			REFERENCES [Origin](id),
+		FOREIGN KEY (origin_id) 
+		REFERENCES [Origin](id),
 	CONSTRAINT FK_Course_History 
-			FOREIGN KEY (course_cod) 
-			REFERENCES [course](code)
+		FOREIGN KEY (course_cod) 
+		REFERENCES [course](code)
 );
+
+
+GO 
+CREATE PROCEDURE setearCorrelatividades
+AS
+	UPDATE course
